1b4edeca55689d94704c58c26478df8629203a5e
Mikołaj R <mikolaj.rodkiewicz@gmail.com>
2018-06-06 23:46:18 +0200

    add a android lib and a 2 framnts view for main activity

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,27 +1,32 @@
 package com.udacity.gradle.builditbigger;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
+import com.example.myjokeandroidlib.JokeFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.main_container)
+    FrameLayout mainContainer;
+    @BindView(R.id.ad_continer)
+    FrameLayout adContiner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.main_container, new MainFragment())
-                    .commit();
-        }
-
+        getSupportFragmentManager().beginTransaction().replace(adContiner.getId(), new JokeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(mainContainer.getId(), new JokeFragment()).commit();
     }
-
-
 
 
 }
