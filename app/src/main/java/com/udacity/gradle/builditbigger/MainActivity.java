2ff1a3c3e136d5b8bfe7e543caff3c9d686a2390
Mikołaj R <mikolaj.rodkiewicz@gmail.com>
2018-06-07 18:22:47 +0200

    add a admob

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,10 +4,13 @@ package com.udacity.gradle.builditbigger;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.FrameLayout;
 
 import com.example.myjokeandroidlib.JokeFragment;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,15 +20,26 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_container)
     FrameLayout mainContainer;
-    @BindView(R.id.ad_continer)
+    @BindView(R.id.adView)
     @Nullable
-    FrameLayout adContiner;
+    AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        if (BuildConfig.FLAVOR == "free") {
+
+            MobileAds.initialize(this,
+                    "ca-app-pub-3940256099942544~3347511713");
+
+
+            adView = findViewById(R.id.adView);;
+            AdRequest adRequest = new AdRequest.Builder().build();
+            adView.loadAd(adRequest);
+        }
 
         new EndpointsAsyncTask(){
             @Override
@@ -39,16 +53,13 @@ public class MainActivity extends AppCompatActivity {
         }.execute();
     }
 
-    public void startFragment(String joke){
+    public void startFragment(String joke) {
         Bundle bundle = new Bundle();
-        bundle.putString(JokeFragment.BUNDLE_JOKE,joke);
+        bundle.putString(JokeFragment.BUNDLE_JOKE, joke);
         JokeFragment jokeFragment = new JokeFragment();
         jokeFragment.setArguments(bundle);
 
-        if (BuildConfig.FLAVOR == "free") {
-            adContiner.setVisibility(View.VISIBLE);
-            getSupportFragmentManager().beginTransaction().replace(adContiner.getId(), jokeFragment).commit();
-        }
+
 
         getSupportFragmentManager().beginTransaction().replace(mainContainer.getId(), jokeFragment).commit();
     }
